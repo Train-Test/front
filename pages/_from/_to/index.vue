f56938586aa61af3e7a40be06104ecc971e67062
Vivian Sarazin <vivian.sarazin@gmail.com>
2020-12-02 23:07:18 +0100

    feat: :sparkles: adding some details to the itin. page

--- pages/_from/_to/index.vue
+++ pages/_from/_to/index.vue
@@ -1,10 +1,77 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="text-4xl">From {{ result.from }} to {{ result.to }}</h1>
+  <div>
+    <div class="container">
+      <div>
+        <h1 class="text-4xl">From {{ result.from }} to {{ result.to }}</h1>
+      </div>
+      <div>
+        <h2 class="text-2xl">Figures</h2>
+        <p>Distance: {{ result.distance }}km</p>
+      </div>
+      <div class="grid grid-cols-3">
+        <div>
+          <h3 class="text-xl">Plane</h3>
+          <p>Duration: {{ result.plane.duration }}h</p>
+          <p>Fare: {{ result.plane.fare }}€</p>
+          <p>CO²: {{ result.plane.co2 }}g</p>
+        </div>
+        <div>
+          <h3 class="text-xl">Car</h3>
+          <p>Duration: {{ result.car.duration }}h</p>
+          <p>Fare: {{ result.car.fare }}€</p>
+          <p>CO²: {{ result.car.co2 }}g</p>
+        </div>
+        <div>
+          <h3 class="text-xl">Train (currently)</h3>
+          <p>Duration: {{ result.currentTrain.duration }}h</p>
+          <p>Fare: {{ result.currentTrain.fare }}€</p>
+          <p>CO²: {{ result.currentTrain.co2 }}g</p>
+        </div>
+      </div>
     </div>
-    <div>
-      <h2 class="text-2xl">Figures</h2>
+    <div class="container">
+      <div>
+        <h2 class="text-4xl">
+          What if we had a high speed train line between those cities?
+        </h2>
+      </div>
+      <div>
+        <h2 class="text-xl">A journey</h2>
+        <div class="grid grid-cols-3">
+          <p class="text-lg text-center">
+            Duration: {{ result.futureTrain.duration }}h
+          </p>
+          <p class="text-lg text-center">
+            Fare: {{ result.futureTrain.fare }}€
+          </p>
+          <p class="text-lg text-center">CO²: {{ result.futureTrain.co2 }}g</p>
+        </div>
+      </div>
+      <div>
+        <h2 class="text-xl">CO² savings</h2>
+        <div class="grid grid-cols-3">
+          <p class="text-lg text-center">
+            Daily {{ result.futureTrain.savings.daily }} T
+          </p>
+          <p class="text-lg text-center">
+            Monthly {{ result.futureTrain.savings.monthly }} T
+          </p>
+          <p class="text-lg text-center">
+            Yearly {{ result.futureTrain.savings.yearly }} T
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2 class="text-xl">Details</h2>
+        <div class="grid grid-cols-2">
+          <p class="text-lg text-center">
+            Potential delivery by {{ result.futureTrain.delivery }}
+          </p>
+          <p class="text-lg text-center">
+            Estimated amount: {{ result.futureTrain.amount }}€
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +86,33 @@ export default {
       result: {
         from: '',
         to: '',
+        distance: '',
+        plane: {
+          duration: '',
+          fare: '',
+          co2: '',
+        },
+        car: {
+          duration: '',
+          fare: '',
+          co2: '',
+        },
+        currentTrain: {
+          duration: '',
+          fare: '',
+          co2: '',
+        },
+        futureTrain: {
+          delivery: 2030,
+          duration: '',
+          fare: '',
+          co2: '',
+          savings: {
+            daily: '',
+            monthly: '',
+            yearly: '',
+          },
+        },
       },
       cities: [
         { name: 'Bordeaux', id: 'BOD' },
@@ -38,6 +132,34 @@ export default {
       return {
         from: this.getCityName(from),
         to: this.getCityName(to),
+        distance: Math.round(Math.random() * 10000),
+        plane: {
+          duration: Math.round(Math.random() * 3),
+          fare: Math.round(Math.random() * 100),
+          co2: Math.round(Math.random() * 10),
+        },
+        car: {
+          duration: Math.round(Math.random() * 5),
+          fare: Math.round(Math.random() * 100),
+          co2: Math.round(Math.random() * 10),
+        },
+        currentTrain: {
+          duration: Math.round(Math.random() * 3),
+          fare: Math.round(Math.random() * 100),
+          co2: Math.round(Math.random() * 10),
+        },
+        futureTrain: {
+          delivery: 2030,
+          amount: Math.round(Math.random() * 3000000),
+          duration: Math.round(Math.random() * 3),
+          fare: Math.round(Math.random() * 100),
+          co2: Math.round(Math.random() * 5),
+          savings: {
+            daily: Math.round(Math.random() * 100),
+            monthly: Math.round(Math.random() * 100 * 30),
+            yearly: Math.round(Math.random() * 100 * 30 * 12),
+          },
+        },
       }
     },
   },
