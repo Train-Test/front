dfe783b33b6718c7ca55ff1412cad17cb62c9131
Vivian Sarazin <vivian.sarazin@gmail.com>
2020-12-02 22:32:44 +0100

    feat: :zap: working on the search feat

--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,36 @@
     <div>
       <h1 class="text-6xl">Train Test</h1>
     </div>
-    <div>
-      <label class="text-lg">From</label>
-      <multiselect
-        v-model="from"
-        :options="options"
-        placeholder="Select one"
-        label="name"
-        track-by="name"
-      ></multiselect>
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <label class="text-lg">From</label>
+        <multiselect
+          v-model="from"
+          :options="options"
+          placeholder="Select one"
+          label="name"
+          track-by="name"
+        ></multiselect>
+      </div>
+      <div>
+        <label class="text-lg">To</label>
+        <multiselect
+          v-model="to"
+          :options="options.filter((el) => el.name !== from.name)"
+          placeholder="Select one"
+          label="name"
+          track-by="name"
+        ></multiselect>
+      </div>
     </div>
-    <div>
-      <label class="text-lg">To</label>
-      <multiselect
-        v-model="to"
-        :options="options"
-        placeholder="Select one"
-        label="name"
-        track-by="name"
-      ></multiselect>
-    </div>
-    <div>
-      <button class="text-white text-center m-2 bg-green-500 p-4 rounded-lg">
-        Hello
-      </button>
+    <div v-if="from.id && to.id" class="text-center">
+      <nuxt-link :to="`${from.id}/${to.id}`">
+        <button
+          class="text-white text-center m-2 bg-green-500 px-4 py-2 rounded-lg"
+        >
+          {{ from.cta }}
+        </button>
+      </nuxt-link>
     </div>
   </div>
 </template>
@@ -43,11 +49,11 @@ export default {
       from: { name: 'Pick a city', id: '' },
       to: { name: 'Pick a city', id: '' },
       options: [
-        { name: 'Bordeaux', id: 'BOD' },
-        { name: 'Lyon', id: 'LYS' },
-        { name: 'Madrid', id: 'MAD' },
-        { name: 'Berlin', id: 'BER' },
-        { name: 'Rome', id: 'ROM' },
+        { name: 'Bordeaux', cta: 'Allez !', id: 'BOD' },
+        { name: 'Lyon', cta: 'Allez !', id: 'LYS' },
+        { name: 'Madrid', cta: 'Vamos!', id: 'MAD' },
+        { name: 'Berlin', cta: 'Los!', id: 'BER' },
+        { name: 'Rome', cta: 'Dai', id: 'ROM' },
       ],
     }
   },
